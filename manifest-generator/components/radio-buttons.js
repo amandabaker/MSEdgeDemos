f3d68bb17bc3f6643f422b14ac5f9aba39cba2b6
Kristin <71902467+Kbhlee2121@users.noreply.github.com>
2023-09-12 18:08:20 -0700

    Update color picker; update defaults.css; add radio buttons component (#16)
    * Adding text radio buttons (no styling)
    
    * update default style
    
    * update color picker
    
    * add styles to example
    
    * add style and value to radio button
    
    * update format
    
    ---------
    
    Co-authored-by: Alexander Kyereboah <alex.kyereboah@gmail.com>

---
+++ manifest-generator/components/radio-buttons.js
@@ -0,0 +1,65 @@
+// Component for text label radio buttons
+// Usage:
+// <radio-buttons options="option1,option2,option3"></radio-buttons>
+
+class RadioButtons extends HTMLElement {
+  constructor() {
+    super();
+
+    // Create a shadow root
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const tableWrapper = document.createElement("div");
+    tableWrapper.setAttribute("class", "table");
+
+    // Parse attribute for radio items
+    let array = this.getAttribute("options").split(",");
+    // Create a label/radio button for each item
+    array.forEach((element) => {
+      let tableItem = document.createElement("div");
+      // Create radio button and append
+      let radioButton = document.createElement("input");
+      radioButton.setAttribute("type", "radio");
+      radioButton.setAttribute("name", "radio-group");
+      radioButton.setAttribute("value", element);
+      tableItem.append(radioButton);
+      // Create radio button label and append
+      let radioLabel = document.createElement("label");
+      radioLabel.textContent = element;
+      tableItem.append(radioLabel);
+      tableWrapper.append(tableItem);
+    });
+
+    // Style the elements
+    const style = document.createElement("style");
+    style.textContent = `.tableitem {
+        align-self: center;
+      }
+    
+      .table {
+        display: flex;
+        flex-direction: column;
+      }`;
+
+    const stylesheetDefault = document.createElement("link");
+    stylesheetDefault.setAttribute("rel", "stylesheet");
+    stylesheetDefault.setAttribute("href", "../styles/defaults.css");
+    shadow.append(stylesheetDefault);
+
+    // Append the table and style to the shadow DOM
+    shadow.append(tableWrapper);
+    shadow.append(style);
+  }
+
+  getUserInput() {
+    const inputElement = this.shadowRoot.querySelector(
+      "input[type='radio']:checked"
+    );
+    if (inputElement) {
+      return inputElement.value;
+    }
+  }
+}
+
+// Define the new element
+customElements.define("radio-buttons", RadioButtons);
